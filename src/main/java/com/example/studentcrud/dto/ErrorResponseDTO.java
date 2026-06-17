5170d1cbe807f1766672b2ee9e5d109739fa978a
neeth <=neethucheriyan2005@gmail.com>
2026-06-17 15:17:04 +0530

    error rersponse

---
+++ src/main/java/com/example/studentcrud/dto/ErrorResponseDTO.java
@@ -0,0 +1,30 @@
+package com.example.studentcrud.dto;
+
+public class ErrorResponseDTO {
+        private String message;
+        private int status;
+
+        public ErrorResponseDTO() {
+        }
+
+        public ErrorResponseDTO(String message, int status) {
+            this.message = message;
+            this.status = status;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+}
--- src/main/java/com/example/studentcrud/service/StudentService.java
+++ src/main/java/com/example/studentcrud/service/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.studentcrud.service;
 
+import com.example.studentcrud.dto.ErrorResponseDTO;
 import com.example.studentcrud.dto.StudentRequestDTO;
 import com.example.studentcrud.dto.StudentResponseDTO;
 import com.example.studentcrud.entity.Student;
@@ -23,7 +24,7 @@ public class StudentService {
         String result = StudentValidation.validate(request);
 
         if(!result.equals("VALID")) {
-            return result;
+            return new ErrorResponseDTO(result, 400);
         }
 
         Student student = new Student();
